894930f872437ff0c463e570d66d9a3e4046af44
chenqi <chenqi92104@icloud.com>
2024-04-07 10:11:25 +0800

    init

---
+++ src/main/java/cn/allbs/phototool/PhotoToolApplication.java
@@ -0,0 +1,13 @@
+package cn.allbs.phototool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/allbs/phototool/PhotoToolApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.allbs.phototool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
